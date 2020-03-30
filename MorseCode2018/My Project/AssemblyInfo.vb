24763f920ec807b4422302076becc85266ee20e0
joe mccarthy-holland <joe.mch@gmail.com>
2020-03-30 13:43:11 +0100

    Add project files.

---
+++ MorseCode2018/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MorseCode2018")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Godalming College")> 
+<Assembly: AssemblyProduct("MorseCode2018")> 
+<Assembly: AssemblyCopyright("Copyright © Godalming College 2018")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e3e180ba-c965-4a03-a6b2-d7e373761572")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
